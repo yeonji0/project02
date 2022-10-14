7ce7ba545b70b01ffbaddcb48a91395363658c31
yeonji0 <kyj0279@gmail.com>
2022-10-14 18:04:05 +0900

    s

--- src/components/EduSlide.vue
+++ src/components/EduSlide.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="container">
+  <div class="container mb-5">
     <div class="eduWrap">
       <h1>JA 교육 프로그램</h1>
       <div class="menuWrap">
           <ul>
-            <li><a href="#">진로취업</a></li>
-            <li><a href="#">경제금융</a></li>
-            <li><a href="#">기업가정신</a></li>
-            <li><a href="#">디지털 리터러시</a></li>
+            <li>진로취업</li>
+            <li>경제금융</li>
+            <li>기업가정신</li>
+            <li>디지털 리터러시</li>
           </ul>
       </div>
     </div>
+
     <swiper 
     :navigation="true" 
     :modules="modules" class="mySwiper">
-      <swiper-slide v-for="(item,i) in eduData" :key="i">
-        {{item.name}}
 
+      <swiper-slide v-for="(item,i) in eduData" :key="i" class="slide p-5">
+        <div class="txtWrap p-5">
+            <h2 v-html="item.name" ></h2>
+            <h2 v-html="item.title" class="mb-3"></h2>
+            <p v-html="item.subtitle" class="mb-5"></p>
+            <button class="btn" >자세히보기 <span><i class="fa-solid fa-arrow-right"></i></span></button>
+        </div>
+        <div class="imgWrap">
+            <img :src="`./img/03eduProgram/${item.src}`" alt="" class="img-fluid">
+        </div>
       </swiper-slide>
+      
     </swiper>
   </div>
 </template>
@@ -49,12 +59,12 @@ export default {
 .eduWrap {
   display: flex;
   gap:50px;
-  h1{font-weight: bold;}
+  font-weight: bold;
   .menuWrap{
     ul{display: flex; padding: 0;height: 100%;}
     li{
         height: 100%; display: flex; justify-content: center; align-items: center;
-        a{display: flex; padding:0 20px;}
+        display: flex; padding:0 20px;
     }
     
   }
@@ -62,5 +72,20 @@ export default {
 }
 .mySwiper {
   height: 500px;
+  display: flex;
+  .slide{display: flex; gap:50px;}
+
+}
+.txtWrap{
+     .btn{
+                margin: 0 40px;
+                width: 200px; height: 60px;
+                background: #fff;
+                border-radius: 50px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.5);
+                display: flex;justify-content: center; align-items: center;
+                padding: 0 20px;
+                span{margin-left: 20px;}
+            }
 }
 </style>
