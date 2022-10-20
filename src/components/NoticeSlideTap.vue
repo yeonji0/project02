6d0d07360bc48d7b4a46749d6b5ed0433d58deac
yeonji0 <kyj0279@gmail.com>
2022-10-20 14:47:33 +0900

    d

--- src/components/NoticeSlideTap.vue
+++ src/components/NoticeSlideTap.vue
@@ -1,26 +1,30 @@
 <template>
-  <swiper 
-    v-bind="swiperOptions1"
-    :modules="modules1" 
-    class="mySwiper1">
-
-      <swiper-slide v-if="tab==1" v-for="item in notice" :key="item" class="slide p-5" :class="{notice:tab=1}">
-        <h4>{{item.name}}</h4>
-        <div class="title">{{item.title}}</div>
-        <p v-html="item.content"></p>
-      </swiper-slide>
-
-      <swiper-slide v-if="tab==2" v-for="item in story" :key="item" class="slide p-5" :class="{notice:tab=2}">
-        <h4>{{item.name}}</h4>
-         <div class="title">{{item.title}}</div>
-      </swiper-slide>
-
-      <swiper-slide v-if="tab==3" v-for="item in news" :key="item" class="slide p-5" :class="{notice:tab=3}">
-        <h4>{{item.name}}</h4>
-         <div class="title">{{item.title}}</div>
-      </swiper-slide>
-
-    </swiper>
+  <div class="tapWrap">
+    <swiper
+      v-bind="swiperOptions1"
+      :modules="modules1"
+      class="mySwiper1 p-5">
+        <swiper-slide v-if="tab==1" v-for="item in notice" :key="item" class="slide p-5" :class="{notice:tab=1}">
+          <h4>{{item.name}}</h4>
+          <div class="title">{{item.title}}</div>
+          <p v-html="item.content"></p>
+        </swiper-slide>
+        <swiper-slide v-if="tab==2" v-for="item in story" :key="item" class="slide p-5" :class="{notice:tab=2}">
+          <h4>{{item.name}}</h4>
+           <div class="title">{{item.title}}</div>
+           <p v-html="item.subtitle"></p>
+        </swiper-slide>
+        <swiper-slide v-if="tab==3" v-for="item in news" :key="item" class="slide p-5" :class="{notice:tab=3}">
+          <h4>{{item.name}}</h4>
+           <div class="title">{{item.title}}</div>
+           <p v-html="item.content"></p>
+        </swiper-slide>
+      </swiper>
+      <div class="btnWrap">
+        <div class="prev"><img :src="`./img/prev.png`" alt=""></div>
+        <div class="next"><img :src="`./img/next.png`" alt=""></div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -48,10 +52,14 @@ export default {
       modules1: [Navigation,Pagination],
       swiperOptions1:{
         slidesPerView:3,
-            spaceBetween:30,
-            loop:true,
-            observer: true,
-            observeParents: true,
+        spaceBetween:30,
+        loop:true,
+        observer: true,
+        observeParents: true,
+         navigation:{
+          nextEl:'.tapWrap .next',
+          prevEl:'.tapWrap .prev',
+        },
       }
     };
   },
@@ -59,9 +67,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.tapWrap{
+  position: relative;
+  .btnWrap{
+    width: 100%;
+    position: absolute;
+    display: flex; justify-content: space-between;
+    top: 50%;
+    z-index: 100;
+    .next, .prev{
+      width: 70px;height: 50px;
+      display: flex;justify-content: center;align-items: center;
+      background: #fff; border-radius: 30px;
+      box-shadow: 0 0 10px rgba(0,0,0,0.3);
+      cursor: pointer;
+      }
+  }
+
+}
 .mySwiper1{
-.swiper-slide{
-    width: 200px;
+  height: 500px;
+.slide{
+    width: 100px; height: 100%;
     h4{
         background: #000; color: #fff;
         width: 80px;height: 30px;
@@ -69,7 +96,14 @@ export default {
         display: flex; justify-content: center;align-items: center;
         border-radius: 50px;
     }
-    .title{font-weight: bold; font-size: 1.5vw;}
+    .title{
+      font-weight: bold; font-size: 1.3vw;
+      height: 80px;
+      overflow: hidden;
+      text-overflow: hidden;
+      }
+    p{text-overflow: hidden; height: 93px; overflow: hidden;}
     }
 }
+
 </style>
